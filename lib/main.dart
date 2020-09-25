d6c148716c46bc6001b5591d2c5704f5bfbf2e80
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-09-25 16:33:46 +0530

    Fix text theme spacing
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    final textTheme = GoogleFonts.latoTextTheme(Theme.of(context).textTheme);
+    final newTextTheme = textTheme
+        .apply(
+          bodyColor: Colors.blueGrey[900],
+        )
+        .copyWith(
+          overline: textTheme.overline.copyWith(
+            fontSize: 12,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText1: textTheme.bodyText1.copyWith(letterSpacing: 1.0),
+          bodyText2: textTheme.bodyText2.copyWith(letterSpacing: 1.0),
+          headline1: textTheme.headline1.copyWith(letterSpacing: 1.0),
+          headline2: textTheme.headline2.copyWith(letterSpacing: 1.0),
+          headline3: textTheme.headline3.copyWith(letterSpacing: 1.0),
+          headline4: textTheme.headline4.copyWith(letterSpacing: 1.0),
+          headline5: textTheme.headline5.copyWith(letterSpacing: 1.0),
+          headline6: textTheme.headline6.copyWith(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.0,
+          ),
+        );
     return MaterialApp(
       title: 'DotMyConcepts',
       theme: ThemeData(
@@ -23,14 +45,7 @@ class MyApp extends StatelessWidget {
           elevation: 1,
           color: Colors.white,
           brightness: Brightness.light,
-          textTheme: GoogleFonts.latoTextTheme(
-            textTheme.copyWith(
-              headline6: textTheme.headline6.copyWith(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
+          textTheme: newTextTheme,
           iconTheme: Theme.of(context).iconTheme,
         ),
         scaffoldBackgroundColor: Colors.white,
@@ -38,19 +53,7 @@ class MyApp extends StatelessWidget {
           color: Colors.black38,
           space: 1,
         ),
-        textTheme: GoogleFonts.latoTextTheme(
-          textTheme
-              .apply(
-                bodyColor: Colors.blueGrey[900],
-              )
-              .copyWith(
-                overline: textTheme.overline.copyWith(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w700,
-                ),
-                bodyText1: textTheme.bodyText1.copyWith(letterSpacing: 1.0),
-              ),
-        ),
+        textTheme: newTextTheme,
       ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
