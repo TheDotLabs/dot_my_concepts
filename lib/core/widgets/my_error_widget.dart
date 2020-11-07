d6bafe8834068f54d3680818f887f88076db6f22
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-11-07 20:36:32 +0530

    Fix cast exception when showing api error
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- lib/core/widgets/my_error_widget.dart
+++ lib/core/widgets/my_error_widget.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_record_lesson/utils/constants.dart';
 
 class MyErrorWidget extends StatelessWidget {
-  final String error;
+  final dynamic error;
 
   const MyErrorWidget(this.error);
 
   @override
   Widget build(BuildContext context) {
-    return Text(error ?? Constants.somethingWentWrong);
+    return Text(error?.toString() ?? Constants.somethingWentWrong);
   }
 }
